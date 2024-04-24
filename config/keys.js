a4f4a9eca5b4c2bc7761d96fb59bdd4405ec8982
Streple <streple@streple.com>
2024-04-24 16:10:50 -0400

    all

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword33 = 'mongodb://localhost:27017/mern2';
+dbPassword = 'mongodb+srv://cryptolennox:HelloToday12@cluster0.sgfznzl.mongodb.net/CryptoLennoxUsers'
+
+module.exports = {
+    mongoURI: dbPassword
+};
